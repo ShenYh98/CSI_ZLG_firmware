2e2bc668332c2cd5f0abe649ea651e4aaff76cf1
shenyh <907184203@qq.com>
2023-07-27 10:05:32 +0800

    1. 添加自动化工程构建
    Signed-off-by: shenyh <907184203@qq.com>

---
+++ make.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# 检查脚本参数
+if [ "$1" != "x86" ] && [ "$1" != "clean" ]; then
+  echo "请输入一个参数，可选参数为 x86 或 clean"
+  exit 1
+fi
+
+# 进入build目录
+cd build
+
+# 根据输入的参数选择操作
+if [ $1 = "clean" ]; then
+  # 清除构建
+  rm -rf *
+  echo "构建已清除"
+elif [ $1 = "x86" ]; then
+  # 进行cmake构建
+  if [ -n "$2" ]; then
+    cmake -DLOG_LEVEL="$2" ..
+  else
+    cmake ..
+  fi
+
+  # 检查cmake是否成功
+  if [ $? -eq 0 ]; then
+    # 使用make编译程序
+    make
+
+    # 检查make编译是否成功
+    if [ $? -eq 0 ]; then
+      echo "编译成功"
+    else
+      echo "make编译失败"
+      exit 1
+    fi
+  else
+    echo "cmake构建失败"
+    exit 1
+  fi
+else
+  echo "参数错误，请输入 x86 或 clean"
+  exit 1
+fi
