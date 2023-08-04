8373e7f87a5f8d763e7e68e683457e080ce3d55c
shenyh <907184203@qq.com>
2023-08-05 03:12:37 +0800

    1. part test change commit

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 检查脚本参数
-if [ "$1" != "x86" ] && [ "$1" != "clean" ]; then
-  echo "请输入一个参数，可选参数为 x86 或 clean"
+if [ "$1" != "x86" ] && [ "$1" != "clean" ] && [ "$1" != "arm" ]; then
+  echo "请输入一个参数，可选参数为 x86 或 arm 或 clean"
   exit 1
 fi
 
@@ -22,6 +22,34 @@ if [ $1 = "clean" ]; then
   rm -rf *
   echo "构建已清除"
 elif [ $1 = "x86" ]; then
+  # 进行cmake构建
+  if [ -n "$2" ]; then
+    cmake -DLOG_LEVEL="$2" ..
+  else
+    cmake ..
+  fi
+
+  # 检查cmake是否成功
+  if [ $? -eq 0 ]; then
+    # 使用make编译程序
+    make
+
+    # 检查make编译是否成功
+    if [ $? -eq 0 ]; then
+      echo "编译成功"
+    else
+      echo "make编译失败"
+      exit 1
+    fi
+  else
+    echo "cmake构建失败"
+    exit 1
+  fi
+elif [ $1 = "arm" ]; then
+  # 使用交叉编译工具链进行编译
+  # 确保交叉编译工具链已安装并位于正确的路径下
+  export CXX=aarch64-linux-gnu-g++
+
   # 进行cmake构建
   if [ -n "$2" ]; then
     cmake -DLOG_LEVEL="$2" ..
--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,28 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
+#include <cstring>
 
-#include "cjson/cJSON.h"
-#include "cjson/cJSON_Utils.h"
+#include "nlohmann/json.hpp"
 
-using namespace std;
+using json = nlohmann::json;
+
+void parseJSONFile(const std::string& filename) {
+    // 打开JSON文件
+    std::ifstream file(filename);
+    std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    file.close();
+
+    // 解析JSON数据
+    json jsonData = json::parse(content);
+
+    // 获取字段的值
+    std::string name = jsonData["name"];
+    int password = jsonData["password"];
+}
 
 int main() {
-    std::cout << "version 0.1" << endl;
-
-    std::string path = "./config/csi_config.json";
-
-    // 读取 JSON 文件
-    std::ifstream file(path);
-    if (!file.is_open()) {
-        std::cout << "Failed to open JSON file." << std::endl;
-        return 0;
-    } else {
-        // 将内容写到json_data里
-        std::stringstream buffer;
-        buffer <<file.rdbuf();
-        std::string json_data = buffer.str();
-
-        // 解析JSON字符串
-        cJSON* root = cJSON_Parse(json_data.c_str());
-        
-        // 检查是否解析成功
-        if (root == nullptr) {
-            std::cout << "JSON解析错误: " << cJSON_GetErrorPtr() << std::endl;
-            return 0;
-        }
-        
-        // 获取JSON属性值
-        cJSON* json_msg = cJSON_GetObjectItem(root, "msg");
-        if (json_msg != nullptr && cJSON_IsString(json_msg)) {
-            std::string msg = json_msg->valuestring;
-            std::cout << "msg: " << msg << std::endl;
-        }
-
-        // 释放JSON资源
-        cJSON_Delete(root);
-        file.close();
-    }
+    std::string filename = "/var/www/html/json/user.json";
+    parseJSONFile(filename);
 
     return 0;
 }
