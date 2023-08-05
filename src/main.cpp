2796adf2d4321cddb2f5b86097fa5d1bba893d66
shenyh <907184203@qq.com>
2023-08-04 21:46:28 -0700

    1.加入websocket库
    2.修改cmake，链接boost库

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,41 @@ void parseJSONFile(const std::string& filename) {
     int password = jsonData["password"];
 }
 
+
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+
+using websocketpp::server;
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+
+using websocketpp::connection_hdl;
+using websocketpp::config::asio;
+
+void on_message(connection_hdl hdl, server<asio>::message_ptr msg) {
+    std::string message = msg->get_payload();
+    std::cout << "收到客户端消息：" << message << std::endl;
+    
+    // 进行消息处理或响应
+    // ...
+}
+
 int main() {
-    std::string filename = "/var/www/html/json/user.json";
-    parseJSONFile(filename);
+    using namespace std::placeholders;
+    
+    server<asio> echo_server;
+    
+    try {
+        echo_server.set_message_handler(std::bind(&on_message, _1, _2));
+
+        echo_server.init_asio();
+        echo_server.listen(9002);
+
+        echo_server.start_accept();
+        echo_server.run();
+    } catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
 
     return 0;
 }
